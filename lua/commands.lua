95782d9420ef7e4c0f94268ebb7a9018228dfac7
John Woodruff <rocketpanda40@gmail.com>
2020-10-24 02:37:01 -0400

    Add ability to get dupe or info table from ReadTable(). Fix file output of table. Begin work on re-encoding table.

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,18 +1,25 @@
-function ReadTable(dupe)
+function ReadTable(dupe,tbl)
 	local s,t,i =AdvDupe2.Decode(file.Read(dupe))
-	return t
+	if tbl =="t" then return t elseif tbl =="i" then return i else return {} end
 end
 
 concommand.Add("ad2t_print", function(ply,cmd,args)
-	PrintTable(ReadTable("advdupe2/"..args[1]..".txt"))
+	PrintTable(ReadTable("advdupe2/"..args[1]..".txt","t"))
 end)
 
 concommand.Add("ad2t_decode", function(ply,cmd,args)
-	file.Write("ad2t/"..args[1]..".txt", ReadTable("advdupe2/"..args[1]..".txt"))
+	file.Write("ad2t/"..args[1]..".txt", util.TableToJSON((ReadTable("advdupe2/"..args[1]..".txt","t")), true))
 end)
 
---[[
 concommand.Add("ad2t_encode", function(ply,cmd,args)
+	--[[
+	AdvDupe2.Encode(util.JSONToTable(file.Read("ad2t/"..args[1]..".txt")),ReadTable("advdupe2/"..args[1]..".txt",i),function(data)
+		AdvDupe2.SendToClient(ply,data,0)
+	end)
+	]]
 
+	AdvDupe2.Encode(util.JSONToTable(file.Read("ad2t/"..args[1]..".txt")),AdvDupe2.GenerateDupeStamp(ply),function(data)
+		AdvDupe2.SendToClient(ply,data,0)
+        end)
 end)
-]]
+
