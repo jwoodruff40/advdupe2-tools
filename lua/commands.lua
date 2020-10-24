6d0a1fbdd39552baf78ad7ded657e830712e4386
John Woodruff <rocketpanda40@gmail.com>
2020-10-24 14:14:18 -0400

    Add ability to rename new dupe and place it in advdupe2 folder. Emulate ternary in base_dir assignment.

--- lua/commands.lua
+++ lua/commands.lua
@@ -17,6 +17,10 @@ concommand.Add("ad2t_decode", function(ply,cmd,args)
 end)
 
 concommand.Add("ad2t_encode", function(ply,cmd,args)
-	file.Write("ad2t/"..args[1]..".txt",AdvDupe2.ovEncode(util.JSONToTable(file.Read("ad2t/"..args[1].."data.txt")),util.JSONToTable(file.Read("ad2t/"..args[1].."info.txt"))))
+	args[2] =args[2] or args[1]
+	args[3] =args[3] or false
+	base_dir =(args[3]) and "advdupe2/" or "ad2t/"
+
+	file.Write(base_dir..args[2]..".txt",AdvDupe2.ovEncode(util.JSONToTable(file.Read("ad2t/"..args[1].."data.txt")),util.JSONToTable(file.Read("ad2t/"..args[1].."info.txt"))))
 end)
 
