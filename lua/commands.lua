be586094266d888e33ba44474b563c3b3fb34911
kerbalcadet <kerbalcadet@gmail.com>
2020-10-24 03:22:04 -0500

    basic functionality

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,24 +1,23 @@
+include "filesys.lua"
+
 function ReadTable(dupe,tbl)
-	local s,t,i =AdvDupe2.Decode(file.Read(dupe))
-	if tbl =="t" then return t elseif tbl =="i" then return i else return {} end
+	return AdvDupe2.Decode(file.Read(dupe))
 end
 
 concommand.Add("ad2t_print", function(ply,cmd,args)
-	PrintTable(ReadTable("advdupe2/"..args[1]..".txt","t"))
+	local s, t, i = ReadTable("advdupe2/"..args[1]..".txt")
+	PrintTable(t)
 end)
 
 concommand.Add("ad2t_decode", function(ply,cmd,args)
-	file.Write("ad2t/"..args[1]..".txt", util.TableToJSON((ReadTable("advdupe2/"..args[1]..".txt","t")), true))
+	local s, t, i = ReadTable("advdupe2/"..args[1]..".txt")
+	CheckDir("ad2t/"..args[1])
+	file.Write("ad2t/"..args[1].."info.txt", util.TableToJSON(i), true)
+	file.Write("ad2t/"..args[1].."data.txt", util.TableToJSON(t), true)
 end)
 
 concommand.Add("ad2t_encode", function(ply,cmd,args)
-	--[[
-	AdvDupe2.Encode(util.JSONToTable(file.Read("ad2t/"..args[1]..".txt")),ReadTable("advdupe2/"..args[1]..".txt",i),function(data)
-		AdvDupe2.SendToClient(ply,data,0)
-	end)
-	]]
-
-	AdvDupe2.Encode(util.JSONToTable(file.Read("ad2t/"..args[1]..".txt")),AdvDupe2.GenerateDupeStamp(ply),function(data)
+	AdvDupe2.Encode(util.JSONToTable(file.Read("ad2t/"..args[1].."data.txt")),util.JSONToTable(file.Read("ad2t/"..args[1].."info.txt")),function(data)
 		AdvDupe2.SendToClient(ply,data,0)
         end)
 end)
--- lua/filesys.lua
+++ lua/filesys.lua
@@ -1,4 +1,9 @@
 if !file.Exists("ad2t", "DATA") then
 	file.CreateDir("ad2t")
-	file.Write(dir, "")
+end
+
+function CheckDir(dir)
+	if !file.Exists(dir, "DATA") then
+	file.CreateDir(dir)
+	end
 end
