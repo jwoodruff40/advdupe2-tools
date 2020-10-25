8090be328b33397ef2dfa32f0c37e3a9470e6905
John Woodruff <rocketpanda40@gmail.com>
2020-10-24 23:50:26 -0400

    Fix TableToJSON prettyPrint

--- lua/commands.lua
+++ lua/commands.lua
@@ -12,8 +12,8 @@ end)
 concommand.Add("ad2t_decode", function(ply,cmd,args)
 	local s, t, i = ReadTable("advdupe2/"..args[1]..".txt")
 	CheckDir("ad2t/"..args[1])
-	file.Write("ad2t/"..args[1].."info.txt", util.TableToJSON(i), true)
-	file.Write("ad2t/"..args[1].."data.txt", util.TableToJSON(t), true)
+	file.Write("ad2t/"..args[1].."info.txt", util.TableToJSON(i,true))
+	file.Write("ad2t/"..args[1].."data.txt", util.TableToJSON(t,true))
 end)
 
 concommand.Add("ad2t_encode", function(ply,cmd,args)
@@ -23,4 +23,3 @@ concommand.Add("ad2t_encode", function(ply,cmd,args)
 	
 	AdvDupe2.Encode(util.JSONToTable(file.Read("ad2t/"..args[1].."data.txt")),util.JSONToTable(file.Read("ad2t/"..args[1].."info.txt")),function(encString) file.Write(base_dir..args[2]..".txt",encString) end)
 end)
-
