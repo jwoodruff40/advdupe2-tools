969cd6134b90457bc33acb5fa161233a600195bc
John Woodruff <rocketpanda40@gmail.com>
2020-10-25 13:14:07 -0400

    Refactor creating subdirs into mkDirs(). Allow saving encoded file to subdir(s).

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,22 +1,27 @@
 include "filesys.lua"
 
-function ReadTable(dupe)
+local function ReadTable(dupe)
 	return AdvDupe2.Decode(file.Read(dupe))
+end
+
+local function mkDirs(path)
+	local sb, se = string.find(path, "/[^/]+$", 1)
+	local folder = string.sub(path, 1, sb)
+
+	CheckDir(folder)
 end
 
 concommand.Add("ad2t_print", function(ply,cmd,args)
 	local s, t, i = ReadTable("advdupe2/"..args[1]..".txt")
+
 	PrintTable(t)
 end)
 
 concommand.Add("ad2t_decode", function(ply,cmd,args)
 	local s, t, i = ReadTable("advdupe2/"..args[1]..".txt")
-
-	local sb, se = string.find("ad2t/"..args[1], "/[^/]+$", 1)
-	local folder = string.sub("ad2t/"..args[1], 1, sb)
-	CheckDir(folder)
-
 	local wt = {info = i, data = t}
+
+	mkDirs("ad2t/"..args[1])
 	file.Write("ad2t/"..args[1]..".txt", util.TableToJSON(wt, true))
 
 	print("outputted to garrysmod/data/"..folder)
@@ -28,7 +33,8 @@ concommand.Add("ad2t_encode", function(ply,cmd,args)
 	local base_dir =tobool(args[3]) and "advdupe2/" or "ad2t/"
 	local rt = util.JSONToTable(file.Read("ad2t/"..args[1]..".txt"))
 	
-	AdvDupe2.Encode(rt.data,rt.info, function(encString) 
+	AdvDupe2.Encode(rt.data,rt.info, function(encString)
+		mkDirs(base_dir..args[2])
 		file.Write(base_dir..args[2]..".txt",encString) 
 	end)
 
